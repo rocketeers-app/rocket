4246fbdaf77d3a267422638fe753910d74da4787
Mark van Eijk <mark@vaneijk.co>
2023-07-25 13:46:34 +0200

    wip

--- app/Actions/UseHerdOrValet.php
+++ app/Actions/UseHerdOrValet.php
@@ -2,6 +2,7 @@
 
 namespace App\Actions;
 
+use Exception;
 use Lorisleiva\Actions\Concerns\AsAction;
 use Symfony\Component\Process\Process;
 
@@ -12,9 +13,16 @@ class UseHerdOrValet
     public function handle()
     {
         $process = Process::fromShellCommandline('(type herd > /dev/null && echo "herd") || (type valet > /dev/null && echo "valet")');
-        $process->setTty(Process::isTtySupported());
         $process->run();
 
-        return trim($process->getOutput());
+        $output = trim($process->getOutput());
+
+        if (str_contains($output, 'herd')) {
+            return 'herd';
+        } elseif (str_contains($output, 'valet')) {
+            return 'valet';
+        }
+
+        throw new Exception('No Herd or Valet found');
     }
 }
