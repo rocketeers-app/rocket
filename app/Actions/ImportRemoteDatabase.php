34b287b00d642832dba60cbac80e51c4ab848133
Mark van Eijk <mark@vaneijk.co>
2024-12-11 10:47:23 +0100

    Update ImportRemoteDatabase.php

--- app/Actions/ImportRemoteDatabase.php
+++ app/Actions/ImportRemoteDatabase.php
@@ -54,7 +54,7 @@ class ImportRemoteDatabase
         $process = Process::fromShellCommandline("mysql -u root --password='' -e 'CREATE DATABASE IF NOT EXISTS `'".$name."'` CHARACTER SET utf8 COLLATE utf8_general_ci'");
         $process->run();
 
-        $process = Process::fromShellCommandline('ssh rocketeer@'.$server.' "sudo mysqldump --host=\''.$host.'\' --user=\''.$username.'\' --password=\''.$password.'\' --no-tablespaces \''.$database.'\' | sudo gzip" | gunzip | mysql -u root --password=\'\' \''.$name.'\'');
+        $process = Process::fromShellCommandline('ssh rocketeer@'.$server.' "sudo mysqldump --host=\''.$host.'\' --user=\''.$username.'\' --password=\''.$password.'\' --no-tablespaces \''.$database.'\' | sudo gzip" | gunzip | mysql --user root --password=\'\' --execute \'SET FOREIGN_KEY_CHECKS = 0;\' \''.$name.'\'');
         $process->setTty(Process::isTtySupported());
         $process->setTimeout(3600);
         $process->run();
