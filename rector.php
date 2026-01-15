5299e58438a73f255bb6c82cbb28640a1d4a6a5e
Mark van Eijk <mark@ux.nl>
2026-01-15 20:18:14 +0100

    upgraded to latest versions

--- rector.php
+++ rector.php
@@ -19,7 +19,7 @@ return static function (RectorConfig $rectorConfig): void {
 
     // define sets of rules
     $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_81,
+        LevelSetList::UP_TO_PHP_84,
         SetList::CODE_QUALITY,
         SetList::DEAD_CODE,
         SetList::EARLY_RETURN,
